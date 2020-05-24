c48cf22b700f17a28bc7dcba9e34712eeaa6d5d6
Diego Lascano <diegolascanodalgo@gmail.com>
2020-05-24 18:07:03 -0500

    initial commit. Authentication base working

---
+++ test_driver/app.dart
@@ -0,0 +1,18 @@
+//import 'package:flutter_auth_base/main.dart' as app;
+import 'package:flutter_auth_base/main.dart';
+import 'package:flutter_auth_base/services/apple_sign_in_available.dart';
+import 'package:flutter_auth_base/services/auth_service_adapter.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_driver/driver_extension.dart';
+
+void main() {
+  // This line enables the extension.
+  enableFlutterDriverExtension();
+
+  // Call the `main()` function of the app, or call `runApp` with
+  // any widget you are interested in testing.
+  runApp(MyApp(
+    initialAuthServiceType: AuthServiceType.mock,
+    appleSignInAvailable: AppleSignInAvailable(false),
+  ));
+}
